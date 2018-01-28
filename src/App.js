26f0de667e16c0866701f91a2bedccc78aa3e3ba
LB <mail@lb.ee>
2018-01-28 13:58:51 +1000

    move data transform to one place

--- src/App.js
+++ src/App.js
@@ -31,6 +31,46 @@ import {
   Polyline
 } from "react-google-maps";
 
+const flightsWithLocations = transportItems
+  .filter(item => item.method === "Flight")
+  .map(item => {
+    let flight = Object.assign({}, item);
+    flight.toAirport = airportItems.find(
+      item => item.airportCode === flight.toAirport
+    );
+    flight.fromAirport = airportItems.find(
+      item => item.airportCode === flight.fromAirport
+    );
+    flight.interval = Interval.fromDateTimes(
+      DateTime.fromISO(flight.fromDatetime),
+      DateTime.fromISO(flight.toDatetime)
+    );
+    const airlineLogo = flight.provider
+      .trim()
+      .toLowerCase()
+      .replace(" ", "-");
+    flight.airlineLogoLocation = `/img/airlines/${airlineLogo}.svg`;
+    return flight;
+  });
+
+const travelStats = {
+  totalFlights: flightsWithLocations.length,
+  totalDistanceFlown: flightsWithLocations.reduce(
+    (total, item) => total + item.distanceKilometers,
+    0
+  ),
+  totalMinutesFlown: flightsWithLocations.reduce(
+    // change to a duration and represent as hours, minutes
+    (total, item) => total + item.timeMinutes,
+    0
+  ),
+  totalDifferentAirlines: flightsWithLocations.reduce(
+    (arr, item) =>
+      arr.includes(item.provider) ? arr : arr.concat([item.provider]),
+    []
+  ).length
+};
+
 const ButtonLink = ({ label, to, activeOnlyWhenExact }) => (
   <Route
     path={to}
